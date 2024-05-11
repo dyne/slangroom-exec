0106f7a138b531e59d8c03f66062f2a3016a2301
puria <puria@dyne.org>
2024-05-11 18:52:05 +0700

    test: Initial testing with helpers

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import { Slangroom } from "@slangroom/core";
-
 import { ethereum } from "@slangroom/ethereum";
 import { fs } from "@slangroom/fs";
 import { git } from "@slangroom/git";
@@ -10,32 +9,53 @@ import { oauth } from "@slangroom/oauth";
 import { pocketbase } from "@slangroom/pocketbase";
 import { qrcode } from "@slangroom/qrcode";
 import { redis } from "@slangroom/redis";
+import type { ZenParams } from "@slangroom/shared";
 import { shell } from "@slangroom/shell";
 import { timestamp } from "@slangroom/timestamp";
 import { wallet } from "@slangroom/wallet";
 import { zencode } from "@slangroom/zencode";
 
-for await (const chunk of Bun.stdin.stream()) {
-  // chunk is Uint8Array
-  // this converts it to text (assumes ASCII encoding)
-  const the_input = Buffer.from(chunk).toString();
-  const s = new Slangroom([
-    ethereum,
-    fs,
-    git,
-    helpers,
-    http,
-    JSONSchema,
-    oauth,
-    pocketbase,
-    qrcode,
-    redis,
-    shell,
-    timestamp,
-    wallet,
-    zencode,
-  ]);
+const the_input = await Bun.stdin.text();
+const s = new Slangroom([
+  ethereum,
+  fs,
+  git,
+  helpers,
+  http,
+  JSONSchema,
+  oauth,
+  pocketbase,
+  qrcode,
+  redis,
+  shell,
+  timestamp,
+  wallet,
+  zencode,
+]);
 
-  const { result } = await s.execute(the_input, {});
-  console.log(JSON.stringify(result));
-}
+const decode_and_trim = (r: string) =>
+  Buffer.from(r, "base64").toString().trim();
+const decode = (r: string) => Buffer.from(r, "base64").toString();
+const decode_and_json = (r: string) => JSON.parse(decode(r));
+type Names = "conf" | "data" | "keys" | "extra";
+
+const decode_param = (source: string, name: Names, fn: Function) => {
+  if (source && source !== "") {
+    try {
+      opts[name] = fn(source);
+    } catch (e) {
+      console.error(`${name} is malformed`);
+      process.exit(2);
+    }
+  }
+};
+
+const [c, sl, d, k, e, cx] = the_input.split("\n");
+const opts: ZenParams = { data: {}, keys: {} };
+const contract = decode(sl);
+decode_param(c, "conf", decode_and_trim);
+decode_param(d, "data", decode_and_json);
+decode_param(k, "keys", decode_and_json);
+decode_param(e, "extra", decode_and_json);
+const { result } = await s.execute(contract, opts);
+await Bun.write(Bun.stdout, JSON.stringify(result));
--- test/test.bats
+++ test/test.bats
@@ -11,18 +11,27 @@ setup() {
 }
 
 @test "should execute simple zencode" {
-    run ./slangroom-exec < test/fixtures/simple_zencode.zen
+    load_fixture "simple_zencode"
+    run_slangroom_exec
     assert_output '{"output":["Welcome_to_slangroom-exec_🥳"]}'
     assert_success
 }
 
 @test "should execute simple slangroom" {
-    run ./slangroom-exec < test/fixtures/simple_slangroom.zen
-    assert_output --partial timestamp
+    load_fixture "simple_slangroom"
+    run_slangroom_exec
+    assert_output --partial 'timestamp'
 
     # check that ts is a number
     ts=$(echo $output | jq '.timestamp')
     assert_regex "$ts" '^[0-9]+$'
+    assert_success
+}
 
+@test "should read data correctly" {
+    export FILES_DIR="."
+    load_fixture "read_file"
+    run_slangroom_exec
+    assert_output --partial "Do you know who greets you? 🥒"
     assert_success
 }
